a5d34f677665bd55d0041f6465d56138f516945e
Bhupesh Dahiya <149587292+basesixtyfour@users.noreply.github.com.>
2026-03-10 07:48:34 +0000

    chore: change dockerfile to copy backend folder before running server

--- ecomm_backend/ecomm_backend/settings.py
+++ ecomm_backend/ecomm_backend/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'ecomm_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('DB_PATH', BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
